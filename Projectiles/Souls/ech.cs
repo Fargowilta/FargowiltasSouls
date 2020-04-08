f7b14b010a852d91fc4c48c9c4f3e352dc29af5c
InvalidOS <35383068+Pokecreeper@users.noreply.github.com>
2020-04-08 12:54:09 -0400

    ech projectile

---
+++ Projectiles/Souls/ech.cs
@@ -0,0 +1,70 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Projectiles.Souls
+{
+	public class Ech : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			ProjectileID.Sets.Homing[projectile.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 50;
+			projectile.height = 50;
+			projectile.friendly = true;
+			projectile.tileCollide = false;
+			projectile.ignoreWater = true;
+			projectile.timeLeft = 120;
+		}
+		
+		public override void AI()
+		{
+			projectile.rotation = Math.Atan2(-projectile.velocity.y, projectile.velocity.x);
+			if (projectile.localAI[0] == 0f)
+			{
+				AdjustMagnitude(ref projectile.velocity);
+				projectile.localAI[0] = 1f;
+			}
+			float distance = 400f;
+			bool target = false;
+			Vector2 move = Vector2.Zero;
+			for (int k = 0; k < 200; k++)
+			{
+				if (Main.npc[k].active && !Main.npc[k].dontTakeDamage && !Main.npc[k].friendly && Main.npc[k].lifeMax > 5)
+				{
+					Vector2 newMove = Main.npc[k].Center - projectile.Center;
+					float distanceTo = (float)Math.Sqrt(newMove.X * newMove.X + newMove.Y * newMove.Y);
+					if (distanceTo < distance)
+					{
+						move = newMove;
+						distance = distanceTo;
+						target = true;
+					}
+				}
+			}
+				
+			if (target)
+			{
+				AdjustMagnitude(ref move);
+				projectile.velocity = (10 * projectile.velocity + move) / 11f;
+				AdjustMagnitude(ref projectile.velocity);
+			}
+		}
+
+		private void AdjustMagnitude(ref Vector2 vector)
+		{
+			float magnitude = (float)Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+			if (magnitude > 6f)
+			{
+				vector *= 6f / magnitude;
+			}
+		}
+	}
+}
