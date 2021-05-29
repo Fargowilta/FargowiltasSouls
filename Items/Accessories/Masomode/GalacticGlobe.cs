0258dd21048cf2d436da94d82fb238a84ce49a6f
Lin <63439268+Lin-cat@users.noreply.github.com>
2021-05-29 16:43:44 +0800

    Update GalacticGlobe.cs

--- Items/Accessories/Masomode/GalacticGlobe.cs
+++ Items/Accessories/Masomode/GalacticGlobe.cs
@@ -19,11 +19,12 @@ Summons the true eyes of Cthulhu to protect you
 Increases flight time by 100%
 'Always watching'");
             DisplayName.AddTranslation(GameCulture.Chinese, "银河球");
-            Tooltip.AddTranslation(GameCulture.Chinese, @"'时刻注视'
-免疫翻转,不稳定,扭曲和月之诅咒
-允许使用者改变重力
-召唤真·克苏鲁之眼保护你
-增加100%飞行时间");
+            Tooltip.AddTranslation(GameCulture.Chinese, @"使你免疫翻转、不稳定、扭曲和月之诅咒减益
+允许你控制重力
+免疫太空的低重力环境并无视水的阻力
+召唤真·克苏鲁之眼来保护你
+延长100%飞行时间
+'时刻注视着'");
         }
 
         public override void SetDefaults()
