f4e191dd73e2ab623ae00635651f020c0aab869c
Lin <63439268+Lin-cat@users.noreply.github.com>
2021-06-05 13:42:22 +0800

    Update FargoBullet.cs

--- Items/Ammos/FargoBullet.cs
+++ Items/Ammos/FargoBullet.cs
@@ -23,9 +23,9 @@ namespace FargowiltasSouls.Items.Ammos
             DisplayName.AddTranslation(GameCulture.Chinese, "混合子弹袋");
             Tooltip.AddTranslation(GameCulture.Chinese,
                                "追踪敌人\n" +
-                               "弹跳多次\n" +
+                               "可弹跳多次\n" +
                                "每次撞击都会造成魔晶碎片爆炸\n" +
-                               "造成多种Debuff");
+                               "造成多种减益");
         }
 
         public override void SetDefaults()
