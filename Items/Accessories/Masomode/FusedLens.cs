f24252393daa0db4cecca580ca298179df340951
Lin <63439268+Lin-cat@users.noreply.github.com>
2021-06-06 09:10:33 +0800

    Update FusedLens.cs

--- Items/Accessories/Masomode/FusedLens.cs
+++ Items/Accessories/Masomode/FusedLens.cs
@@ -15,9 +15,9 @@ namespace FargowiltasSouls.Items.Accessories.Masomode
 Your attacks inflict Cursed Inferno and Ichor
 'Too melted to improve vision'");
             DisplayName.AddTranslation(GameCulture.Chinese, "融合晶状体");
-            Tooltip.AddTranslation(GameCulture.Chinese, @"'融化过度,无法改善视力'
-免疫诅咒地狱和脓液
-攻击造成诅咒地狱和脓液");
+            Tooltip.AddTranslation(GameCulture.Chinese, @"使你免疫诅咒狱火和灵液减益
+攻击会造成诅咒狱火和灵液减益
+'融化过度，无法改善视力'");
         }
 
         public override void SetDefaults()
