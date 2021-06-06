ef855281a8651c2ad06fa77e10c9aee5ab2f0a5f
Lin <63439268+Lin-cat@users.noreply.github.com>
2021-06-06 10:04:52 +0800

    Update RabiesVaccine.cs

--- Items/Misc/RabiesVaccine.cs
+++ Items/Misc/RabiesVaccine.cs
@@ -1,6 +1,7 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Localization;
 
 namespace FargowiltasSouls.Items.Misc
 {
@@ -10,6 +11,8 @@ namespace FargowiltasSouls.Items.Misc
         {
             DisplayName.SetDefault("Rabies Vaccine");
             Tooltip.SetDefault(@"Permanently grants immunity to Feral Bite");
+            DisplayName.AddTranslation(GameCulture.Chinese, "狂犬病疫苗");
+            Tooltip.AddTranslation(GameCulture.Chinese, @"使你永久免疫野性噬咬减益");
         }
 
         public override void SetDefaults()
