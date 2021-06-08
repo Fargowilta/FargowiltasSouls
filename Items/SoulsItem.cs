40001efcbd6c00a53ab58de75491b9c5404ac3fe
Lin <63439268+Lin-cat@users.noreply.github.com>
2021-06-08 16:28:37 +0800

    Update SoulsItem.cs

--- Items/SoulsItem.cs
+++ Items/SoulsItem.cs
@@ -86,7 +86,14 @@ namespace FargowiltasSouls.Items
 
             // Add the Eternity toolip after tooltip modification in order to be displayed underneath any manual tooltips (i.e. SoE cycling).
             if (Eternity)
+            if (Language.ActiveCulture == GameCulture.Chinese)
+            {
+                tooltips.Add(new TooltipLine(mod, $"{mod.Name}:永恒", "永恒"));
+            }
+            else
+            {
                 tooltips.Add(new TooltipLine(mod, $"{mod.Name}:Eternity", "Eternity"));
+            }
         }
     }
 }
