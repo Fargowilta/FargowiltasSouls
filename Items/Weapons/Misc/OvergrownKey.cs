cbcbbf085224521717a3330eb9cb92552654225e
Lin <63439268+Lin-cat@users.noreply.github.com>
2021-06-09 14:40:21 +0800

    Update OvergrownKey.cs

--- Items/Weapons/Misc/OvergrownKey.cs
+++ Items/Weapons/Misc/OvergrownKey.cs
@@ -14,7 +14,7 @@ namespace FargowiltasSouls.Items.Weapons.Misc
             DisplayName.SetDefault("Overgrown Key");
             Tooltip.SetDefault("Summons a Jungle Mimic to fight for you\nNeeds 2 minion slots");
             DisplayName.AddTranslation(GameCulture.Chinese, "繁茂之匙");
-            Tooltip.AddTranslation(GameCulture.Chinese, "召唤一个丛林宝箱怪为你而战\n每个宝箱怪占用2格召唤栏");
+            Tooltip.AddTranslation(GameCulture.Chinese, "召唤一个丛林宝箱怪为你而战\n每个宝箱怪占据2格召唤栏");
             ItemID.Sets.StaffMinionSlotsRequired[item.type] = 2;
         }
 
