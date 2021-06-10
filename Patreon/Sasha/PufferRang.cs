c7b8decb76673a975bddeeeb13b5c910c8df970f
Lin <63439268+Lin-cat@users.noreply.github.com>
2021-06-10 12:17:37 +0800

    Update PufferRang.cs

--- Patreon/Sasha/PufferRang.cs
+++ Patreon/Sasha/PufferRang.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Localization;
 
 namespace FargowiltasSouls.Patreon.Sasha
 {
@@ -13,6 +14,7 @@ namespace FargowiltasSouls.Patreon.Sasha
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("PufferRang");
+            DisplayName.AddTranslation(GameCulture.Chinese, "河豚");
             ProjectileID.Sets.TrailCacheLength[projectile.type] = 6;
             ProjectileID.Sets.TrailingMode[projectile.type] = 2;
         }
