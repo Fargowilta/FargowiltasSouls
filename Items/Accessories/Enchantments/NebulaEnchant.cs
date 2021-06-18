2abb7603c6511a07e637ce85c0f3d19cc7351e52
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2021-06-18 10:48:53 +0800

    Update NebulaEnchant.cs

--- Items/Accessories/Enchantments/NebulaEnchant.cs
+++ Items/Accessories/Enchantments/NebulaEnchant.cs
@@ -18,9 +18,9 @@ Buff booster stacking capped at 2
 'The pillars of creation have shined upon you'");
             DisplayName.AddTranslation(GameCulture.Chinese, "星云魔石");
             Tooltip.AddTranslation(GameCulture.Chinese, 
-@"伤害敌人时有几率生成强化增益
-强化增益最大堆叠上限为2
-'创生之柱照耀着你'");
+@"对敌人造成伤害时有几率生成强化焰
+强化增益堆叠上限为2
+“创生之柱照耀着你”");
         }
 
         public override void SafeModifyTooltips(List<TooltipLine> list)
