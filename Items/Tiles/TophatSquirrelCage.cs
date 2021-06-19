8e861a279894497d549556b893225d37ad1a80a7
夏沫 <86086772+Soammer@users.noreply.github.com>
2021-06-19 09:29:43 +0800

    Update TophatSquirrelCage.cs

--- Items/Tiles/TophatSquirrelCage.cs
+++ Items/Tiles/TophatSquirrelCage.cs
@@ -2,6 +2,7 @@ using FargowiltasSouls.Items.Misc;
 using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
+using Terraria.Localization;
 
 namespace FargowiltasSouls.Items.Tiles
 {
@@ -10,6 +11,7 @@ namespace FargowiltasSouls.Items.Tiles
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Top Hat Squirrel Cage");
+            DisplayName.AddTranslation(GameCulture.Chinese, "高顶礼帽松鼠笼"); 
         }
 
         public override void SetDefaults()
