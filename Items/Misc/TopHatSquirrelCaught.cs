bb9413953abbb63ee3f4654ce1e3bb8cf434470f
夏沫 <86086772+Soammer@users.noreply.github.com>
2021-06-19 17:10:38 +0800

    Update TopHatSquirrelCaught.cs

--- Items/Misc/TopHatSquirrelCaught.cs
+++ Items/Misc/TopHatSquirrelCaught.cs
@@ -1,5 +1,6 @@
-﻿using FargowiltasSouls.NPCs.Critters;
+using FargowiltasSouls.NPCs.Critters;
 using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace FargowiltasSouls.Items.Misc
@@ -11,6 +12,7 @@ namespace FargowiltasSouls.Items.Misc
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Top Hat Squirrel");
+            DisplayName.AddTranslation(GameCulture.Chinese, "高顶礼帽松鼠"); 
         }
 
         public override void SetDefaults()
