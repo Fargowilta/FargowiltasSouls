f28ec8723f2b66f4c021edec0f11178716e14656
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2021-06-19 11:01:02 +0800

    Update TikiEnchant.cs

--- Items/Accessories/Enchantments/TikiEnchant.cs
+++ Items/Accessories/Enchantments/TikiEnchant.cs
@@ -17,8 +17,8 @@ namespace FargowiltasSouls.Items.Accessories.Enchantments
 'Aku Aku!'");
             DisplayName.AddTranslation(GameCulture.Chinese, "提基魔石");
             Tooltip.AddTranslation(GameCulture.Chinese,
-@"在召唤栏用光后你仍可以召唤临时的哨兵和仆从
-'Aku Aku!'");
+@"召唤栏位用光后你仍可以召唤临时的哨兵和仆从
+“Aku Aku!”");
         }
 
         public override void SafeModifyTooltips(List<TooltipLine> list)
