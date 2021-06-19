a0a85b6c4167ec6875ba35e44e7a245f704f3ece
夏沫 <86086772+Soammer@users.noreply.github.com>
2021-06-19 09:22:23 +0800

    Update SummonVariants.cs

--- Items/Weapons/SummonVariants/SummonVariants.cs
+++ Items/Weapons/SummonVariants/SummonVariants.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
+using Terraria.Localization;
 
 namespace FargowiltasSouls.Items.Weapons.SummonVariants
 {
