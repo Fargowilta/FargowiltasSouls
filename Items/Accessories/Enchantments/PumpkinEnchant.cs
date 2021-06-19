3521a926925041304b2b75ed4d5adffa6dd0c3d1
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2021-06-19 10:30:11 +0800

    Update PumpkinEnchant.cs

--- Items/Accessories/Enchantments/PumpkinEnchant.cs
+++ Items/Accessories/Enchantments/PumpkinEnchant.cs
@@ -22,7 +22,7 @@ Enemies that touch them will destroy them and take damage
 @"你在地上行走时会种下南瓜
 南瓜成熟时会为你回复25点生命值并产生伤害性余烬
 敌人与南瓜接触时会摧毁南瓜但会受到伤害
-'你对南瓜的突发渴望永远不会得到满足'");
+“你对南瓜的突发渴望永远不会得到满足”");
         }
 
         public override void SafeModifyTooltips(List<TooltipLine> list)
