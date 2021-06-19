7952a4e1d3114369b4f5180c599dbf8d3e647f07
FallCloud <51817522+DawnofEast@users.noreply.github.com>
2021-06-19 17:36:42 +0800

    Update LihzahrdCurse.cs

--- Buffs/Masomode/LihzahrdCurse.cs
+++ Buffs/Masomode/LihzahrdCurse.cs
@@ -9,6 +9,8 @@ namespace FargowiltasSouls.Buffs.Masomode
         {
             DisplayName.SetDefault("Lihzahrd Curse");
             Description.SetDefault("Wires disabled in Jungle Temple");
+            DisplayName.AddTranslation(GameCulture.Chinese, "蜥蜴人的诅咒");
+            Description.AddTranslation(GameCulture.Chinese, "神庙中电路被禁用");
             Main.debuff[Type] = true;
             Main.pvpBuff[Type] = true;
             Main.buffNoSave[Type] = true;
