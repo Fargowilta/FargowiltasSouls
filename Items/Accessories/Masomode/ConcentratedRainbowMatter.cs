f02710541473b8ac99be795bae0685981b039749
夏沫 <86086772+Soammer@users.noreply.github.com>
2021-06-20 10:18:04 +0800

    Update ConcentratedRainbowMatter.cs

--- Items/Accessories/Masomode/ConcentratedRainbowMatter.cs
+++ Items/Accessories/Masomode/ConcentratedRainbowMatter.cs
@@ -16,9 +16,9 @@ namespace FargowiltasSouls.Items.Accessories.Masomode
 Summons a baby rainbow slime to fight for you
 'Taste the rainbow'");
             DisplayName.AddTranslation(GameCulture.Chinese, "浓缩彩虹物质");
-            Tooltip.AddTranslation(GameCulture.Chinese, @"'品尝彩虹'
-免疫宇宙之火
-召唤一个彩虹史莱姆宝宝");
+            Tooltip.AddTranslation(GameCulture.Chinese, @"免疫宇宙之火
+召唤一个彩虹史莱姆宝宝
+'品尝彩虹'");
         }
 
         public override void SetDefaults()
