b7946efff84319dd6bb8eb0b5633f292de8128d5
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2021-06-20 11:01:47 +0800

    Update ReverseManaFlow.cs

--- Buffs/Masomode/ReverseManaFlow.cs
+++ Buffs/Masomode/ReverseManaFlow.cs
@@ -16,7 +16,7 @@ namespace FargowiltasSouls.Buffs.Masomode
             longerExpertDebuff = false;
             canBeCleared = false;
             DisplayName.AddTranslation(GameCulture.Chinese, "反魔力流");
-            Description.AddTranslation(GameCulture.Chinese, "魔法武器消耗生命,而不是法力");
+            Description.AddTranslation(GameCulture.Chinese, "魔法武器消耗生命，而不是法力");
         }
 
         public override void Update(Player player, ref int buffIndex)
