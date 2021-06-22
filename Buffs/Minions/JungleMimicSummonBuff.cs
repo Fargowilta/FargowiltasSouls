7f331b45b2e2d4e905472612cf43ec074228eb26
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2021-06-22 10:18:09 +0800

    Update JungleMimicSummonBuff.cs

--- Buffs/Minions/JungleMimicSummonBuff.cs
+++ Buffs/Minions/JungleMimicSummonBuff.cs
@@ -9,6 +9,8 @@ namespace FargowiltasSouls.Buffs.Minions
         {
             DisplayName.SetDefault("Jungle Mimic");
             Description.SetDefault("The Jungle Mimic will fight for you");
+            DisplayName.AddTranslation(GameCulture.Chinese, "丛林宝箱怪");
+            Description.AddTranslation(GameCulture.Chinese, "丛林大宝箱怪会为你而战");
             Main.buffNoTimeDisplay[Type] = true;
             Main.buffNoSave[Type] = true;
         }
