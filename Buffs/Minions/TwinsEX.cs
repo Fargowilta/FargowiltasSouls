39e5dabab9a5ae23ffd076cac652a3755512da01
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2021-06-22 10:24:32 +0800

    Update TwinsEX.cs

--- Buffs/Minions/TwinsEX.cs
+++ Buffs/Minions/TwinsEX.cs
@@ -9,6 +9,8 @@ namespace FargowiltasSouls.Buffs.Minions
         {
             DisplayName.SetDefault("Twins EX");
             Description.SetDefault("The real Twins will fight for you");
+            DisplayName.AddTranslation(GameCulture.Chinese, "双子EX");
+            Description.AddTranslation(GameCulture.Chinese, "真正的双子魔眼会为你而战");
             Main.buffNoTimeDisplay[Type] = true;
             Main.buffNoSave[Type] = true;
         }
