3ee32d5997885f1627ce8b0f4081f0f06f3731f5
Anne Leze <39974170+MizusawaMatsuri@users.noreply.github.com>
2021-07-03 13:12:51 +0800

    Update RabiesVaccine.cs

--- Items/Misc/RabiesVaccine.cs
+++ Items/Misc/RabiesVaccine.cs
@@ -1,7 +1,7 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using Terraria.Localization
+using Terraria.Localization;
 
 namespace FargowiltasSouls.Items.Misc
 {
