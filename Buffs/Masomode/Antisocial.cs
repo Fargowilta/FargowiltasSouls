03f53b9b97c91a7b51ae4ed085aaf163098c1996
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2022-01-06 23:00:46 +0800

    Update Antisocial.cs

--- Buffs/Masomode/Antisocial.cs
+++ Buffs/Masomode/Antisocial.cs
@@ -8,14 +8,14 @@ namespace FargowiltasSouls.Buffs.Masomode
     {
         public override void SetDefaults()
         {
-            DisplayName.SetDefault("Antisocial");
-            Description.SetDefault("You have no friends");
+            //DisplayName.SetDefault("Antisocial");
+            //Description.SetDefault("You have no friends");
             Main.debuff[Type] = true;
             Main.pvpBuff[Type] = true;
             Main.buffNoSave[Type] = true;
             canBeCleared = true;
-            DisplayName.AddTranslation(GameCulture.Chinese, "反社交");
-            Description.AddTranslation(GameCulture.Chinese, "你没有朋友");
+            //DisplayName.AddTranslation(GameCulture.Chinese, "反社交");
+            //Description.AddTranslation(GameCulture.Chinese, "你没有朋友");
 
         }
 
