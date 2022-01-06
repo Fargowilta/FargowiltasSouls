04cb9edb782e5f7808efd7d28416ea815e74639b
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2022-01-06 23:01:34 +0800

    Update Atrophied.cs

--- Buffs/Masomode/Atrophied.cs
+++ Buffs/Masomode/Atrophied.cs
@@ -8,14 +8,14 @@ namespace FargowiltasSouls.Buffs.Masomode
     {
         public override void SetDefaults()
         {
-            DisplayName.SetDefault("Atrophied");
-            Description.SetDefault("Your muscles are deteriorating");
+            //DisplayName.SetDefault("Atrophied");
+            //Description.SetDefault("Your muscles are deteriorating");
             Main.debuff[Type] = true;
             Main.pvpBuff[Type] = true;
             Main.buffNoSave[Type] = true;
             canBeCleared = true;
-            DisplayName.AddTranslation(GameCulture.Chinese, "萎缩");
-            Description.AddTranslation(GameCulture.Chinese, "你的肌肉正在退化");
+            //DisplayName.AddTranslation(GameCulture.Chinese, "萎缩");
+            //Description.AddTranslation(GameCulture.Chinese, "你的肌肉正在退化");
         }
 
         public override void Update(Player player, ref int buffIndex)
