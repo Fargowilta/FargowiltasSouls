1dcdac202c8d47dbd50f2eefbda4f8d1e6f6d353
FallinLeaf <97594977+FallinLeaf@users.noreply.github.com>
2022-06-25 18:26:13 +0800

    removed chinese texts of eridanium and EternalEnergy in oringinal files
    because it's outdated and there's new texts in .hjson

--- Items/Materials/Eridanium.cs
+++ Items/Materials/Eridanium.cs
@@ -10,8 +10,8 @@ namespace FargowiltasSouls.Items.Materials
         {
             DisplayName.SetDefault("Eridanium");
             Tooltip.SetDefault("A shard of cosmic power");
-            DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "月之水晶");
-            Tooltip.AddTranslation((int)GameCulture.CultureName.Chinese, "月球能量的碎片\n宇宙英灵掉落");
+            //DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "月之水晶");
+            //Tooltip.AddTranslation((int)GameCulture.CultureName.Chinese, "月球能量的碎片\n宇宙英灵掉落");
             Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 30;
         }
 
--- Items/Materials/EternalEnergy.cs
+++ Items/Materials/EternalEnergy.cs
@@ -14,10 +14,10 @@ namespace FargowiltasSouls.Items.Materials
             DisplayName.SetDefault("Eternal Energy");
             Tooltip.SetDefault(@"Grants immunity to almost all Eternity Mode debuffs
 'Proof of having embraced eternity'");
-            DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "施虐狂");
-            Tooltip.AddTranslation((int)GameCulture.CultureName.Chinese,
-@"'受苦的证明'
-免疫几乎所有受虐模式的Debuff");
+        //DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "施虐狂");
+            //Tooltip.AddTranslation((int)GameCulture.CultureName.Chinese,
+//@"'受苦的证明'
+//免疫几乎所有受虐模式的Debuff");
 
             Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 30;
         }
