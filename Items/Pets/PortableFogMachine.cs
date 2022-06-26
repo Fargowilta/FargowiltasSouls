3848436a3494bd66b047ceab4ca4eff9be14fbab
FallinLeaf <97594977+FallinLeaf@users.noreply.github.com>
2022-06-26 10:50:44 +0800

    new pets!

--- Items/Pets/PortableFogMachine.cs
+++ Items/Pets/PortableFogMachine.cs
@@ -8,24 +8,27 @@ using Terraria.ModLoader;
 
 namespace FargowiltasSouls.Items.Pets
 {
-    public class BabyScythe : SoulsItem
+    public class PortableFogMachine : SoulsItem
     {
+        public override string Texture => "FargowiltasSouls/Items/Placeholder";
+
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Baby Scythe");
-            Tooltip.SetDefault("Summons Baby Abom\n'Don't worry, it's dull'");
+            Tooltip.SetDefault("Summons a baby shadow creature\n'Born in the darkness, molded by it'");
             Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
 
         public override void SetDefaults()
         {
             Item.CloneDefaults(ItemID.DukeFishronPetItem);
-            Item.shoot = ModContent.ProjectileType<BabyAbom>();
-            Item.buffType = ModContent.BuffType<BabyAbomBuff>();
+            Item.shoot = ModContent.ProjectileType<BabySilhouette>();
+            Item.buffType = ModContent.BuffType<BabySilhouetteBuff>();
         }
 
         public override void UseStyle(Player player, Rectangle heldItemFrame)
         {
+            base.UseStyle(player, heldItemFrame);
+
             if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
             {
                 player.AddBuff(Item.buffType, 3600);
