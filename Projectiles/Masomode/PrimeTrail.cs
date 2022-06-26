9a6b48fda4bcae16fac07cd350e1b68964785c8c
FallinLeaf <97594977+FallinLeaf@users.noreply.github.com>
2022-06-26 10:25:26 +0800

    instead they have a trail

--- Projectiles/Masomode/PrimeTrail.cs
+++ Projectiles/Masomode/PrimeTrail.cs
@@ -3,6 +3,7 @@ using FargowiltasSouls.EternityMode.Content.Boss.HM;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -27,6 +28,12 @@ namespace FargowiltasSouls.Projectiles.Masomode
             Projectile.alpha = 255;
             Projectile.aiStyle = -1;
             Projectile.scale = 0.8f;
+            Projectile.hide = true;
+        }
+
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            behindNPCs.Add(index);
         }
 
         public override void AI()
@@ -42,9 +49,14 @@ namespace FargowiltasSouls.Projectiles.Masomode
                     if (!npc.GetEModeNPCMod<PrimeLimb>().IsSwipeLimb || npc.ai[2] < 140)
                         fade = true;
                 }
-                else if (Projectile.ai[1] == 1)
+                else if (Projectile.ai[1] == 1) //attack limb while spinning
                 {
                     if (npc.GetEModeNPCMod<PrimeLimb>().IsSwipeLimb || (Main.npc[(int)npc.ai[1]].ai[1] != 1 && Main.npc[(int)npc.ai[1]].ai[1] != 2))
+                        fade = true;
+                }
+                else if (Projectile.ai[1] == 2) //attack limb doing attacks
+                {
+                    if (npc.GetEModeNPCMod<PrimeLimb>().IsSwipeLimb || Main.npc[(int)npc.ai[1]].ai[1] == 1 || Main.npc[(int)npc.ai[1]].ai[1] == 2)
                         fade = true;
                 }
             }
@@ -72,25 +84,35 @@ namespace FargowiltasSouls.Projectiles.Masomode
 
         public override bool PreDraw(ref Color lightColor)
         {
-            float increment = 0.25f;
+            float increment;
             if (Projectile.ai[1] == 1f)
                 increment = 0.1f;
+            else if (Projectile.ai[1] == 2f)
+                increment = 0.5f;
+            else
+                increment = 0.25f;
+
+            Texture2D glow = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
 
             for (float i = 0; i < ProjectileID.Sets.TrailCacheLength[Projectile.type]; i += increment)
             {
                 int max0 = (int)i - 1;
                 if (max0 < 0)
                     continue;
-                Player player = Main.player[Projectile.owner];
-                Texture2D glow = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
-                Color color27 = (Projectile.ai[1] == 0f ? new Color(255, 0, 0, 210) : new Color(191, 51, 255, 210)) * 0.25f * Projectile.Opacity;
-                if (Projectile.ai[1] == 0f)
-                    color27 *= 0.5f;
+                
+                Color color27;
+                if (Projectile.ai[1] == 1)
+                    color27 = new Color(191, 51, 255, 210); //purple
+                else if (Projectile.ai[1] == 2)
+                    color27 = new Color(51, 255, 191, 210) * 0.75f; //teal
+                else
+                    color27 = new Color(255, 255, 75, 210); //yellow
+
+                color27 *= 0.3f * Projectile.Opacity;
                 color27 *= ((float)ProjectileID.Sets.TrailCacheLength[Projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[Projectile.type];
                 float scale = Projectile.scale;
                 scale *= ((float)ProjectileID.Sets.TrailCacheLength[Projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[Projectile.type];
                 Vector2 center = Vector2.Lerp(Projectile.oldPos[(int)i], Projectile.oldPos[max0], (1 - i % 1));
-                float smoothtrail = i % 1 * (float)Math.PI / 6.85f;
 
                 center += Projectile.Size / 2;
 
