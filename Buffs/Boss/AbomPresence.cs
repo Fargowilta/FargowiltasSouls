4e603798e9f1e9d52e30e3e784c7d933b3c8015c
FallinLeaf <3324888485@qq.com>
2022-07-02 19:50:38 +0800

    MapObject and Boss Debuffs?

--- Buffs/Boss/AbomPresence.cs
+++ Buffs/Boss/AbomPresence.cs
@@ -10,8 +10,8 @@ namespace FargowiltasSouls.Buffs.Boss
         {
             DisplayName.SetDefault("Abominable Presence");
             Description.SetDefault("Defense, damage reduction, and life regen reduced; Moon Leech effect");
-            DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "憎恶驾到");
-            Description.AddTranslation((int)GameCulture.CultureName.Chinese, "减少防御、伤害减免和生命恢复速度;附带月噬减益");
+            //DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "憎恶驾到");
+            //Description.AddTranslation((int)GameCulture.CultureName.Chinese, "减少防御、伤害减免和生命恢复速度;附带月噬减益");
             Main.debuff[Type] = true;
             Main.buffNoSave[Type] = true;
             Main.buffNoTimeDisplay[Type] = true;
--- Buffs/Boss/DeviPresence.cs
+++ Buffs/Boss/DeviPresence.cs
@@ -10,8 +10,8 @@ namespace FargowiltasSouls.Buffs.Boss
         {
             DisplayName.SetDefault("Deviant Presence");
             Description.SetDefault("Friendly NPCs take massively increased damage");
-            DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "戴维安驾到");
-            Description.AddTranslation((int)GameCulture.CultureName.Chinese, "大幅增加友方NPC受到的伤害");
+            //DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "戴维安驾到");
+            //Description.AddTranslation((int)GameCulture.CultureName.Chinese, "大幅增加友方NPC受到的伤害");
             Main.debuff[Type] = true;
             Main.buffNoSave[Type] = true;
 
