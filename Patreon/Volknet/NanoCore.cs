46d80fbadb3262e521eb9774c19f01b81ca774bd
FallinLeaf <3324888485@qq.com>
2022-09-24 11:52:40 +0800

    resolve

--- Patreon/Volknet/NanoCore.cs
+++ Patreon/Volknet/NanoCore.cs
@@ -54,7 +54,6 @@ namespace FargowiltasSouls.Patreon.Volknet
 
         public override void SafeModifyTooltips(List<TooltipLine> tooltips)
         {
-            base.SafeModifyTooltips(tooltips);
             foreach (TooltipLine line2 in tooltips)
             {
                 if (line2.Mod == "Terraria" && line2.Name == "ItemName")
