fbc1c7bbf6ef9c7635bf59ab6d7cae4d26e861bb
Javyz <jacklukas@outlook.com>
2022-12-19 21:30:29 +0100

    Unfinished Prisma Regalia + unfinished HM spear reworks

--- EModePlayer.cs
+++ EModePlayer.cs
@@ -3,6 +3,7 @@ using FargowiltasSouls.NPCs;
 using FargowiltasSouls.Projectiles.Masomode;
 using Microsoft.Xna.Framework;
 using System;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
@@ -27,6 +28,22 @@ namespace FargowiltasSouls
 
         public int ShorterDebuffsTimer;
         public const int MaxShorterDebuffsTimer = 60;
+
+        public List<int> ReworkedSpears = new List<int>
+            {
+                ItemID.Spear,
+                ItemID.AdamantiteGlaive,
+                ItemID.CobaltNaginata,
+                ItemID.MythrilHalberd,
+                ItemID.OrichalcumHalberd,
+                ItemID.PalladiumPike,
+                ItemID.TitaniumTrident,
+                ItemID.Trident,
+                ItemID.ObsidianSwordfish,
+                ItemID.Swordfish,
+                ItemID.ChlorophytePartisan
+            };
+
 
         private int WeaponUseTimer => Player.GetModPlayer<FargoSoulsPlayer>().WeaponUseTimer;
 
@@ -569,6 +586,8 @@ namespace FargowiltasSouls
                     AttackSpeed *= 0.75f;
                     return 1f;
 
+                case var _ when ReworkedSpears.Contains(item.type):
+                    return 2f;
                 default:
                     break;
             }
